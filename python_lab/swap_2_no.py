b9dd628366880728b33783a0f87e950cf7248974
adarshvinod-devs <adarsh2047n@gmail.com>
2026-09-05 15:40:15 +0530

    swap 2 no

---
+++ python_lab/swap_2_no.py
@@ -0,0 +1,12 @@
+num_1 = int(input("Enter num 1 : "))
+num_2 = int(input("Enter num 2 : "))
+
+#num_1 , num_2 = num_2 , num_1
+
+num_1 = num_1 + num_2
+
+num_2 = num_1 - num_2
+
+num_1 = num_1 - num_2
+
+print(f"Num 1 : {num_1}\nNum 2 : {num_2}")
