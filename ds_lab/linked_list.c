e2202428c8ea5bb6598b90139f6fbe31fa751d7f
adarshvinod-devs <adarsh2047n@gmail.com>
2026-09-21 23:05:00 +0530

    Linked List Creation

--- ds_lab/linked_list.c
+++ ds_lab/linked_list.c
@@ -1,4 +1,4 @@
-#include <stdio.h>>
+#include <stdio.h>
 #include <stdlib.h>
 
 struct Node{
@@ -10,9 +10,29 @@ struct Node{
 
 int main(){
     int n;
+    struct Node *newNode, *temp, *head = NULL;
 
     printf("Enter the number of nodes to be created :\n");
     scanf("%d",&n);
+
+    for(int i = 0; i < n; i++){
+        newNode = malloc(sizeof(struct Node));
+        printf("\nEnter data: \n");
+        scanf("%d",&newNode -> data);
+        newNode -> next = NULL;
+
+        if(head == NULL){
+            head = newNode;
+            temp = head;
+        }
+        else{
+            while(temp -> next != NULL){
+                temp = temp -> next;
+            }
+            temp -> next = newNode;
+        }
+
+    }
     
 
 
