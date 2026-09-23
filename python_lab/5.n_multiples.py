216a763373bee83d6d794b546135f1ce4f567357
adarshvinod-devs <adarsh2047n@gmail.com>
2026-09-23 17:54:28 +0530

    fixed bug : replaced multiples with number

--- python_lab/5.n_multiples.py
+++ python_lab/5.n_multiples.py
@@ -1,9 +1,9 @@
 #program 5
 #Find the 1st n multiples of a number
 
-number = input("Enter number :\n")
+number = int(input("Enter number :\n"))
 
 multiples = int(input("Enter number of multiples :\n"))
 
-for i in range(1, multiples):
-    print(i * multiples)
+for i in range(multiples):
+    print((i + 1) * number)
