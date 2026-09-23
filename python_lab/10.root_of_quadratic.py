d6748622db5ca5073a66a7806f62f0082857a006
adarshvinod-devs <adarsh2047n@gmail.com>
2026-09-23 15:09:34 +0530

    program 10

--- python_lab/10.root_of_quadratic.py
+++ python_lab/10.root_of_quadratic.py
@@ -1,1 +1,17 @@
-#progr
+#program 10
+#find roots of quadratic equation
+from math import sqrt
+
+a = int(input("Enter A :"))
+b = int(input("Enter B :"))
+c = int(input("Enter c :"))
+
+b_square_minus_4_ac = (b * b) - (4 * a * c)
+
+numerator_1 = (-b) + sqrt(b_square_minus_4_ac)
+
+numerator_2 = (-b) - sqrt(b_square_minus_4_ac)
+
+denominator = 2 * a
+
+print(f"Solutions ({ numerator_1 / denominator} , { numerator_2 / denominator})")
