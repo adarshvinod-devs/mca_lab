26e2d4cd4e5e0daf4fa1ae567de1f2c380e77dac
adarshvinod-devs <adarsh2047n@gmail.com>
2026-09-23 13:50:14 +0530

    program 6

---
+++ python_lab/6.sum_1st_n_even_no.py
@@ -0,0 +1,20 @@
+#proogram 6
+#Sum of 1st 100 even numbers
+
+the_sum = 0
+count = 0
+is_even = 0
+
+while(is_even <100):
+    count += 1
+    if count % 2 == 0:
+        the_sum = the_sum + count
+        is_even += 1
+
+print(sum)
+
+#better solutions
+print(100 * 101) # n(n + 1)
+
+print(sum(range(2,201,2)))
+
